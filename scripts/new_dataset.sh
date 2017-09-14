c094fe8a7d2fb7300f0a7e9e3ec654c933c807e7
Rob Mealey <robertemealey@gmail.com>
2017-09-14 13:05:35 -0400

    update preprocessing scripts to work with new structure

--- scripts/new_dataset.sh
+++ scripts/new_dataset.sh
@@ -2,54 +2,33 @@ CORPUS=$1
 BASEDIR=$2
 SAMPLE_IDS_PATH=$3
 NUMTOPICS=$4
-MONGO_USER=$5
-MONGO_PW=$6
-MONGO_HOST=$7
-MONGO_PORT=$8
-MONGO_DB=$9
+MALLET_HOME=$5
+NUM_THREADS_MALLET=$6
+MONGO_USER=$7
+MONGO_PW=$8
+MONGO_HOST=$9
+MONGO_PORT=${10}
+MONGO_DB=${11}
 
 TEXTDATAPATH="$BASEDIR/text_data/$CORPUS"
+INPUTPATH="${BASEDIR}/data/${CORPUS}/input/"
+OUTPUTPATH="${BASEDIR}/data/${CORPUS}/output/T${NUMTOPICS}/init/"
+
 rm -r $TEXTDATAPATH
-mkdir $TEXTDATAPATH
-rm -r ${BASEDIR}/WebContent/results/${CORPUS}
-mkdir ${BASEDIR}/WebContent/results/${CORPUS}
+rm -r ${BASEDIR}/data/${CORPUS}
 
-INPUTPATH="${BASEDIR}/WebContent/results/${CORPUS}/input/"
-mkdir $INPUTPATH
-mkdir ${BASEDIR}/WebContent/results/${CORPUS}/log/
-mkdir ${BASEDIR}/WebContent/results/${CORPUS}/output/
-mkdir ${BASEDIR}/WebContent/results/${CORPUS}/output/T${NUMTOPICS}/
-mkdir ${BASEDIR}/WebContent/results/${CORPUS}/output/T${NUMTOPICS}/init/
-
-rm WebContent/data/${CORPUS}.html
-rm WebContent/data/${CORPUS}.titles 
+mkdir -p $TEXTDATAPATH
+mkdir -p $INPUTPATH
+mkdir -p $OUTPUTPATH
 
 python scripts/generate_text_data.py ${SAMPLE_IDS_PATH} ${TEXTDATAPATH} \
     ${MONGO_USER} ${MONGO_PW} ${MONGO_HOST} ${MONGO_PORT} ${MONGO_DB} &
-python scripts/generate_html.py ${SAMPLE_IDS_PATH} WebContent/data/${CORPUS}.html \
+python scripts/generate_html.py ${SAMPLE_IDS_PATH} ${BASEDIR}/data/${CORPUS}.html \
     ${MONGO_USER} ${MONGO_PW} ${MONGO_HOST} ${MONGO_PORT} ${MONGO_DB} &
-python scripts/generate_titles.py ${SAMPLE_IDS_PATH} WebContent/data/${CORPUS}.titles \
+python scripts/generate_titles.py ${SAMPLE_IDS_PATH} ${BASEDIR}/data/${CORPUS}.titles \
     ${MONGO_USER} ${MONGO_PW} ${MONGO_HOST} ${MONGO_PORT} ${MONGO_DB} &
 python scripts/generate_url.py $CORPUS ${SAMPLE_IDS_PATH} ${INPUTPATH}/${CORPUS}.url &
 
 wait
 
-$HOME/tree-TM/bin/mallet import-dir \
-    --input $BASEDIR/text_data/$CORPUS \
-    --output $BASEDIR/WebContent/results/$CORPUS/input/$CORPUS-topic-input.mallet \
-    --remove-stopwords TRUE \
-    --keep-sequence TRUE \
-    --gram-sizes 1,2 \
-    --skip-html TRUE \
-    --keep-sequence-bigrams TRUE
-
-$HOME/tree-TM/bin/mallet train-topics \
-    --input $BASEDIR/WebContent/results/$CORPUS/input/$CORPUS-topic-input.mallet \
-    --num-topics $NUMTOPICS \
-    --topic-word-weights-file $BASEDIR/WebContent/results/$CORPUS/output/T${NUMTOPICS}/init/model.topics \
-    --output-doc-topics $BASEDIR/WebContent/results/$CORPUS/output/T${NUMTOPICS}/init/model.docs \
-    --optimize-interval 10 \
-    --num-threads 30 \
-    --output-topic-keys topic-keys.txt 
-
-
+bash $BASEDIR/scripts/train_mallet.sh $CORPUS $BASEDIR $NUMTOPICS $MALLET_HOME $NUM_THREADS_MALLET
