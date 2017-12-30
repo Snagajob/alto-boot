21a8b2e84d7de2a9800da052d66936628cdc9a63
jkowalewski <jason.kowalewski@snagajob.com>
2017-12-29 21:28:12 -0500

    working docker compose, flyway, postgresql.

--- src/main/java/AltoApplication.java
+++ src/main/java/AltoApplication.java
@@ -5,10 +5,13 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
 @ComponentScan({ "api", "alto", "data" })
+@EnableAutoConfiguration(exclude = { HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
 @SpringBootApplication
 public class AltoApplication {
 
